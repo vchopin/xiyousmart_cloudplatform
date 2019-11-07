43c69f1e8bcc7e2fbeca37c09bc4d39a67dc01cf
linls <1126274414@qq.com>
2019-11-07 16:56:21 +0800

    Initial Commit

---
+++ siteinfo.js
@@ -0,0 +1,11 @@
+var e = {
+    name: "vlinke_cparty",
+    uniacid: "7",
+    acid: "7",
+    multiid: "0",
+    version: "1.0",
+  siteroot: "https://wangbingchao.com/app/index.php",
+    method_design: "3"
+};
+
+module.exports = e;
