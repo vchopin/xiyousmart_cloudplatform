6c9e16eab382f222374625cc9db0bd66a92bc2ce
linls <1126274414@qq.com>
2019-11-07 16:45:27 +0800

    Initial Commit

---
+++ siteinfo.js
@@ -0,0 +1,11 @@
+var e = {
+    name: "vlinke_cparty",
+    uniacid: "5",
+    acid: "5",
+    multiid: "0",
+    version: "2.3.0",
+  siteroot: "https://www.baidu.com/app/index.php",
+    method_design: "3"
+};
+
+module.exports = e;
